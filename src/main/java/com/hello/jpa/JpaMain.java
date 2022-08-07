a031b269022786f4fb5ac22f36e62f7fc18e89d1
seungdols <seungdols0822@gmail.com>
2022-08-07 23:25:10 +0900

    Add main

---
+++ src/main/java/com/hello/jpa/JpaMain.java
@@ -0,0 +1,30 @@
+package com.hello.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("seungdols2");
+
+            entityManager.persist(member);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        } finally {
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+    }
+}
