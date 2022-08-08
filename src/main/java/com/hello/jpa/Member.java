55b802a341a32bb57b525939fc7e0de6afd8dbf3
seungdols <seungdols0822@gmail.com>
2022-08-08 15:52:38 +0900

    Add GeneratedValue table exmaple

--- src/main/java/com/hello/jpa/Member.java
+++ src/main/java/com/hello/jpa/Member.java
@@ -6,15 +6,14 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-@SequenceGenerator(
+@TableGenerator(
         name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ",
-        initialValue = 1,
-        allocationSize = 1
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ", allocationSize = 1
 )
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
     @Column(name = "name", nullable = false)
     private String username;
