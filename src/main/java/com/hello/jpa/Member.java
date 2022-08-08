62d3a6cad0d7606d0dc4c2baab3f84b4b2b0c53d
seungdols <seungdols0822@gmail.com>
2022-08-08 15:22:40 +0900

    Add GeneratedValue sequence exmaple

--- src/main/java/com/hello/jpa/Member.java
+++ src/main/java/com/hello/jpa/Member.java
@@ -6,6 +6,12 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1,
+        allocationSize = 1
+)
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
