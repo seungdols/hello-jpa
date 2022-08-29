3689b2120d83cbe2c614f14b423e0802a316b540
seungdols <seungdols0822@gmail.com>
2022-08-29 21:14:03 +0900

    N:M Mapping

--- src/main/java/com/hello/jpa/Product.java
+++ src/main/java/com/hello/jpa/Product.java
@@ -1,9 +1,8 @@
 package com.hello.jpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Product {
@@ -14,4 +13,30 @@ public class Product {
 
     private String name;
 
+    @ManyToMany(mappedBy = "products")
+    private List<Member> members = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
 }
