f271d55cf93257fbd514b5ddf92c0549fc4fc31f
seungdols <seungdols0822@gmail.com>
2022-08-29 21:12:31 +0900

    N:M Mapping

---
+++ src/main/java/com/hello/jpa/Product.java
@@ -0,0 +1,17 @@
+package com.hello.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+    @Column(name = "PRODUCT_ID")
+    private Long id;
+
+    private String name;
+
+}
