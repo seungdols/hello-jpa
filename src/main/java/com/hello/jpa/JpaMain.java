6e41c7acdae47a65411810b90d68f744635d76b7
seungdols <seungdols0822@gmail.com>
2022-09-02 22:42:25 +0900

    Learn @MappedSuperclass

--- src/main/java/com/hello/jpa/JpaMain.java
+++ src/main/java/com/hello/jpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,6 +15,12 @@ public class JpaMain {
         transaction.begin();
 
         try {
+
+            Member member = new Member();
+            member.setCreatedBy("user1");
+            member.setCreatedAt(LocalDateTime.now());
+
+            entityManager.persist(member);
 
             Movie movie = new Movie();
             movie.setDirector("AA");
