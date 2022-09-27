85f8fd37618b08474da891808fcb9222c32fb473
seungdols <seungdols0822@gmail.com>
2022-09-28 00:00:07 +0900

    Add override equals, hashcode

--- src/main/java/com/hello/jpa/Address.java
+++ src/main/java/com/hello/jpa/Address.java
@@ -1,6 +1,7 @@
 package com.hello.jpa;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Address {
@@ -15,5 +16,18 @@ public class Address {
         this.city = city;
         this.street = street;
         this.zipCode = zipCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipCode, address.zipCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipCode);
     }
 }
